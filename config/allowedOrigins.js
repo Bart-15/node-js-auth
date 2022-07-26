3f6489d0d710f8930222130a7d219758891995d2
Bart-15 <brat.tabusao@gmail.com>
2022-07-26 13:45:02 +0800

    remove nodemon

--- config/allowedOrigins.js
+++
@@ -1,9 +0,0 @@
-const allowedOrigins = [
-    'https://www.yoursite.com',
-    'https://www.google.com',
-    'http://127.0.0.1:5000',
-    'http://localhost:3500',
-    'http://localhost:3000',    
-];
-
-export default allowedOrigins;
