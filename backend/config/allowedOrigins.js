b3f092ec0a0f107451ac9f14774b79d799e1bc4b
Bart-15 <brat.tabusao@gmail.com>
2022-07-27 13:52:04 +0800

    add cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'https://www.google.com',
     'http://127.0.0.1:5000',
     'http://localhost:3500',
-    'http://localhost:3000',    
+    'http://localhost:3000',
+    'https://serene-cucurucho-dfeca4.netlify.app/'    
 ];
 
 export default allowedOrigins;
